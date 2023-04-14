441400ea7e2cb370462fe4f84bad3bddb2045917
DNQGithub <dnq.httt@gmail.com>
2023-04-14 12:53:47 +0700

    - wip

--- client/mobile/src/app/redux/index.ts
+++ client/mobile/src/app/redux/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { authReducer, authSlice } from '@auth/redux';
 import { todoReducer, todoSlice } from '@todo/redux';
 
 export const store = configureStore({
     reducer: {
+        [authSlice.name]: authReducer,
         [todoSlice.name]: todoReducer,
     },
 });
