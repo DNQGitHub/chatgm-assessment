f8c1e928c43951c9d57e62123548071f8f2ee409
DNQGithub <dnq.httt@gmail.com>
2023-04-14 12:55:44 +0700

    - debug

--- client/mobile/src/auth/components/button-connect-metamask/index.tsx
+++ client/mobile/src/auth/components/button-connect-metamask/index.tsx
@@ -4,6 +4,8 @@ import { Button, Text } from 'native-base';
 export const ButtonConnectMetamask = () => {
     const { handleConnect } = useMetamaskContext();
 
+    console.log({ handleConnect });
+
     return (
         <Button colorScheme={'gray'} onPress={handleConnect}>
             <Text color={'white'}>Metamask</Text>
