47cd9c7882637927c0a063e916020bf12435399f
DNQGithub <dnq.httt@gmail.com>
2023-04-14 23:35:00 +0700

    - wip

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@prisma';
 import { TrackingModule } from '@tracking';
 
 @Module({
-    imports: [TrackingModule, PrismaModule],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), TrackingModule, PrismaModule],
 })
 export class AppModule {}
