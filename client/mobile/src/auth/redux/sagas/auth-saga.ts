39ab5b591b9233e12460bdee097fcd3dd19b0abc
Quang Doan <quang.doan@metain.io>
2023-04-15 12:45:34 +0700

    - update tsconfig

--- client/mobile/src/auth/redux/sagas/auth-saga.ts
+++ client/mobile/src/auth/redux/sagas/auth-saga.ts
@@ -2,7 +2,7 @@ import { takeLatest, call } from 'redux-saga/effects';
 import { PayloadAction } from '@reduxjs/toolkit';
 import { authActions } from '@auth/redux/slices';
 import { AuthModel } from '@auth/models';
-import { addConnectWalletEvent } from '../../../api/services';
+import { addConnectWalletEvent } from '@api/services';
 
 function* handleOnAuthSucceded({ payload }: PayloadAction<{ auth: AuthModel }>) {
     console.log('authSaga | handleOnAuthSucceded', { payload });
